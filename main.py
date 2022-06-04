e5af36376ec752e1191532fb416202c284aa4a70
Farih Akmal Haqiqi <gustang@192.168.1.6>
2022-06-05 03:37:23 +0700

    Edit main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from selenium.webdriver.common.by import By
 slug = input("Slug: ")
 url = f"https://www.hackerrank.com/contests/{slug}/leaderboard/"
 data = {
-    'username': [],
-    'nilai': []
+    'name': [],
+    'score': []
 }
 
 driver = webdriver.Safari()
@@ -27,9 +27,8 @@ try:
             name = leader.find_element(By.CLASS_NAME, "leaderboard-hackername")
             score = leader.find_element(By.CLASS_NAME, "span-flex-3")
             print("Add", name.text.strip(), float(score.text.strip())) 
-            data['username'].append(name.text.strip())
-            data['nilai'].append(score.text.strip())
-            # data.append([name.text.strip(), float(score.text.strip())])
+            data['name'].append(name.text.strip())
+            data['score'].append(score.text.strip())
         i += 1
 except:
     pass
@@ -42,4 +41,4 @@ writer = pd.ExcelWriter(f'{slug}.xlsx', engine='xlsxwriter')
 df.to_excel(writer, sheet_name='Sheet1', index=False)
 writer.save()
 
-print(f"DONE...\n{len(data['nilai'])} successfuly added!")    
+print(f"DONE...\n{len(data['score'])} successfuly added!")    
